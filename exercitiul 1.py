0ec3dcafc705ad0bf31b8fecc0a17bcc4c89ef02
oanap <oana.panduru@gmail.com>
2020-09-18 23:36:32 +0300

    first exercise

---
+++ exercitiul 1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 17:50:10 2020
+
+@author: oanap
+"""
+
+#1. Afisati suma tuturor numerelor de 1 la 10.
+x = 0
+y = 1
+n = int(input('enter your number'))
+while y <= n:
+    x = x + y
+    y += 1
+print(x)
+    
+
+
+
+
