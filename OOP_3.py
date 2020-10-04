f994e813cde23941293b5801cad70800bfa3ee3a
oanap <oana.panduru@gmail.com>
2020-10-04 09:34:54 +0300

    with again function

--- OOP_3.py
+++ OOP_3.py
@@ -5,10 +5,10 @@ Created on Sat Oct  3 19:26:31 2020
 @author: oanap
 """
 class New_employee:
-    def __init__(self):
-        self.name = ''
-        self.ID_number = ''
-        self.work_shift = ''
+    #def __init__(self):
+        #self.name = ''
+        #self.ID_number = ''
+        #self.work_shift = ''
     def set_name(self, parameter_name):
         self.name = parameter_name
     def get_name(self):
@@ -21,15 +21,32 @@ class New_employee:
         self.work_shift = work_shift_parameter
     def get_work_shift(self):
         return self.work_shift
-name = input('what is your name?')
-ID_number = int(input('please enter your ID NUMBER:'))
-work_shift = input('please enter your work shift: day shift or night shift?')
-my_employee_info = New_employee()
-print('Here are your employee info:')
-my_employee_info.set_name(name)
-print(my_employee_info.get_name())
-my_employee_info.set_ID_number(ID_number)
-print(my_employee_info.get_ID_number())
-my_employee_info.set_work_shift(work_shift)
-print(my_employee_info.get_work_shift())
+    
+def again_function():
+    response = input('do you want to enter again your details?')
+    while response == 'yes':
+        print('OK, enter again your details.')
+        main()
+        response = input('do you want to enter again your details?')
+    else:
+        print('ok')
+    
+
+def main(): 
+    name = input('what is your name?')
+    ID_number = int(input('please enter your ID NUMBER:'))
+    work_shift = input('please enter your work shift: day shift or night shift?')
+    my_employee_info = New_employee()
+    print('Here are your employee info:')
+    my_employee_info.set_name(name)
+    print(my_employee_info.get_name())
+    my_employee_info.set_ID_number(ID_number)
+    print(my_employee_info.get_ID_number())
+    my_employee_info.set_work_shift(work_shift)
+    print(my_employee_info.get_work_shift())
+main()
+again_function()
+    
+
+
 
