b2528fdf8eb32bc20177cb8a31e3a1fee31b7b25
oanap <oana.panduru@gmail.com>
2020-10-14 22:04:23 +0300

    n factorial

--- recursion.py
+++ recursion.py
@@ -4,12 +4,23 @@ Created on Tue Oct 13 23:03:24 2020
 
 @author: oanap
 """
-
+#method 1
 def my_recursion(n):
     if n == 0:
         return 1
     else:
         return n*my_recursion(n-1)
+    
+n = int(input('enter a number:'))   
+print(my_recursion(n))
+
+
+#method 2
+def my_recursion(n):
+    if n > 0:
+        return n*my_recursion(n-1)
+    else:
+        return 1
     
 n = int(input('enter a number:'))   
 print(my_recursion(n))
